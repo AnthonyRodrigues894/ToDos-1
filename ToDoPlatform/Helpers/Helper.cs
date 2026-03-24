61f572db91babbdddb7bd98b9080f40106cdfd61
GalloJunior <gallojunior@gmail.com>
2026-03-24 10:09:34 -0300

    Criando o login

---
+++ ToDoPlatform/Helpers/Helper.cs
@@ -0,0 +1,20 @@
+using System.Linq.Expressions;
+using System.Net.Mail;
+
+namespace ToDoPlatform.Helpers;
+
+public static class Helper
+{
+    public static bool IsValidEmail(string email)
+    {
+        try
+        {
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch (FormatException)
+        {
+            return false;
+        }
+    }
+}
